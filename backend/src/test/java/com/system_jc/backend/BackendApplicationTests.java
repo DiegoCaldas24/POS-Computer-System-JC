ebbaf67d4ef55cf8c5635c5eaa3447629bf96245
Diego Caldas <juandiegoarmandoc@gmail.com>
2026-04-26 16:50:05 -0500

    feature: initial backend
    backend initialization

---
+++ backend/src/test/java/com/system_jc/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system_jc.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
